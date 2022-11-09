ff4f9b972e498f8b21a10306dce737ab705f08fa
AlexeyOm <alexey.omelchenko@gmail.com>
2022-11-09 21:44:19 +0300

    verbformen finished, css added to model

--- card_maker.py
+++ card_maker.py
@@ -1,9 +1,11 @@
+from typing import List
+
 import bs4 as bs
 import requests
 import genanki
 from model import my_model
 
-input_word = "Erde"
+input_word = "machen"
 
 def main(input:str) ->dict:
     verbformen_not_found = "Немецкие существительные не были найдены"
@@ -14,25 +16,48 @@ def main(input:str) ->dict:
         #TODO реализовать вставку ненайденного слова файл для ручной обработки
         return {"status": 2}
     soup = bs.BeautifulSoup(page_text, features="html.parser")
-    main_word = soup.find("p", class_="vGrnd rCntr").find("b").text
+    main_word = soup.find("p", class_="vGrnd").find("b").text
     if main_word != input:
         #TODO реализовать вставку ненайденного слова в файл для ручной обработки
         return {"status": 3}
 
-    question = soup.find("p", class_="r1Zeile rU3px rO0px").text
+    question = soup.find("p", class_="r1Zeile rU3px rO0px").text.strip()
+    #если нет перевода
+    if not question:
+        return {"status": 4}
+    if soup.find("p", class_="rInf"):
+        word_info = soup.find("p", class_="rInf")
+    else:
+        return {"status": 5}
     #если у нас существительное
-    if soup.find("p", class_="rInf").find("span", {"title" : "существительное"}):
+    #TODO проверка существования файла при записи, генерация имени с префиксом и номером
+    if word_info.find("span", {"title" : "существительное"}):
         translation = soup.find("p", class_="vGrnd rCntr").contents[2].text[1:] + soup.find("p", class_="vGrnd rCntr").find("b").text
         #если есть форма множественного числ
         if len(soup.find("p", class_="vStm rCntr").findAll("b")) == 2:
             translation += " die " + soup.find("p", class_="vStm rCntr").findAll("b")[1].text
-    #TODO добавление глагола
-    #TODO добавление прилагательного
-    #TODO добавление местоимения
+    #если у нас прилагательное
+    elif word_info.find("span", {"title" : "прилагательное"}):
+        translation = soup.find("p", class_="vGrnd rCntr").contents[2].text[1:]
+    elif word_info.find("span", [{"title" : "правильный"}, {"title" : "неправильный"}]):
+        translation = soup.find("p", class_="vGrnd rCntr").contents[2].text[1:].strip()
+    question = question.replace("\n", " ")
+    question = question.replace("а́", "а")
+    question = question.replace("е́", "е")
+    question = question.replace("и́", "и")
+    question = question.replace("о́", "о")
+    question = question.replace("у́", "у")
+    question = question.replace("ы́", "ы")
+    question = question.replace("э́", "э")
+    question = question.replace("я́", "я")
+
+    # TODO добавление прилагательного
+    # TODO добавление местоимения
     #если есть озвучка слова
-    if soup.find("p", class_="vGrnd rCntr").find("a"):
+    if soup.find("p", class_="vGrnd").find("a"):
         print("нашли звук")
-        sound_url = soup.find("p", class_="vGrnd rCntr").find("a").get("href")
+        sound_url = soup.find("p", class_="vGrnd").find_all("a")[-1].get("href")
+        print(f"{sound_url=}")
         response = requests.get(sound_url)
         mp3_file = f"C:\\Users\\Пользователь\\AppData\\Roaming\\Anki2\\Benutzer 1\\collection.media\\{input.lower()}.mp3"
         sound_field = f"[sound:{input.lower()}.mp3]"
@@ -43,23 +68,33 @@ def main(input:str) ->dict:
 
 
 if __name__ == "__main__":
-    result = main(input_word)
+    with open("input.txt", "r") as _:
+        input_words = _.readlines()
+    print(input_words)
     my_deck = genanki.Deck(
         2059400110,
         'Russian > Deutcsh vocab'
     )
     my_package = genanki.Package(my_deck)
     my_package.media_files = []
-    match result["status"]:
-        case 1:
-            my_note = genanki.Note(
-                model=my_model,
-                fields=[result["question"], result["translation"], result["sound"]]
-            )
-            my_deck.add_note(my_note)
-            my_package.media_files.append(result["sound_url"])
-            genanki.Package(my_deck).write_to_file('output/result.apkg')
-        case 2:
-            print("слово не найдено")
-        case 3:
-            print("слово найдено в другой форме")
+    for input_word in input_words:
+        print(input_word)
+        result = main(input_word.strip())
+        match result["status"]:
+            case 1:
+                my_note = genanki.Note(
+                    model=my_model,
+                    fields=[result["question"], result["translation"], result["sound"]]
+                )
+                my_deck.add_note(my_note)
+                my_package.media_files.append(result["sound_url"])
+                genanki.Package(my_deck).write_to_file('output/result.apkg')
+                print(f"{input_word};успешно добавлено")
+            case 2:
+                print(f"{input_word};слово не найдено")
+            case 3:
+                print(f"{input_word};слово найдено в другой форме")
+            case 4:
+                print(f"{input_word};не найден перевод")
+            case 4:
+                print(f"{input_word};что-то не то")
--- model/model.py
+++ model/model.py
@@ -1,5 +1,15 @@
 import genanki
 
+style = """
+.card {
+ font-family: arial;
+ font-size: 24px;
+ text-align: center;
+ color: black;
+ background-color: white;
+}
+"""
+
 my_model = genanki.Model(
     1091735104,
     'Simple Model with Media',
@@ -14,4 +24,8 @@ my_model = genanki.Model(
             'qfmt': '{{Question}}',
             'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}<br>{{MyMedia}}'
         },
-    ])
+    ],
+    css=style,
+    )
+
+#TODO добавить красивый CSS
